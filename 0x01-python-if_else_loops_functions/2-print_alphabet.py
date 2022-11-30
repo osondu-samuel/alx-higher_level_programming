9f6f3993510495e797e8baf35f8e09129c83e6df
osondu-samuel <samuelosondu75softwareengineer@gmail.com>
2022-11-29 19:53:00 -0800

    data structure

---
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+lower = 97
+while lower <= 122:
+    print("{}".format(chr(lower)), end="")
+    lower += 1
---
+++ 0x01-python-if_else_loops_functions/README.md
@@ -0,0 +1,1 @@
+This directory contains my python programs for alx control flow and functions project
